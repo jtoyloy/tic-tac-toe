82bea78c3599f15e9b6343172b07fc0af21b65a4
J.T <jt@MacBook-Pro.cust.communityfibre.co.uk>
2023-04-20 09:16:55 -0400

    added javascript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log('Your JS is linked up. Be the person you needed when you were little.')
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
